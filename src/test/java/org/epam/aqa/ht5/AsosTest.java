3cd4733abd226295f07f3bf395e5e264b7742653
M.Fedoryshyn <fedoryshyn.mykhailo@gmail.com>
2022-09-18 12:11:15 +0300

    Skeleton commit

--- src/test/java/org/epam/aqa/ht5/AsosTest.java
+++ src/test/java/org/epam/aqa/ht5/AsosTest.java
@@ -1,8 +1,11 @@
 package org.epam.aqa.ht5;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class AsosTest {
+public class AsosTest extends BaseTest{
+    private static final String SEARCH_KEYWORD = "product";
+    private static final String EXPECTED_QUERY = "query=product";
 
     @Test
     public void test() {
@@ -17,5 +20,11 @@ public class AsosTest {
     @Test
     public void searchResultTest() {
         System.out.println(3);
+    }
+
+    @Test
+    public void checkThatUrlContainsSearchKeyword() {
+        getHomepage().searchByKeyword(SEARCH_KEYWORD);
+        Assert.assertTrue(getDriver().getCurrentUrl().contains(EXPECTED_QUERY));
     }
 }
